69752e2b67db6e85b4cd69a69a46bca06e400563
Napikip <blogertv01@gmail.com>
2025-05-20 00:21:18 +0300

    server.c

--- server.c
+++ server.c
@@ -5,70 +5,55 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define SERVER_PORT 8080
-#define BUFFER_SIZE 256
-#define MAX_PENDING 5
-
-void setup_server() {
-    int master_socket, client_connection;
-    struct sockaddr_in server_addr;
-    char recv_buffer[BUFFER_SIZE];
-    
-    // Initialize server configuration
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    // Create master socket
-    if ((master_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    // Bind socket to port
-    if (bind(master_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Binding failed");
-        close(master_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    // Start listening
-    if (listen(master_socket, MAX_PENDING) < 0) {
-        perror("Listen failed");
-        close(master_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server running on port %d...\n", SERVER_PORT);
-
-    // Accept incoming connection
-    if ((client_connection = accept(master_socket, NULL, NULL)) < 0) {
-        perror("Accept failed");
-        close(master_socket);
-        exit(EXIT_FAILURE);
-    }
-    printf("New client connected!\n");
-
-    // Receive and process data
-    ssize_t bytes_read = recv(client_connection, recv_buffer, BUFFER_SIZE-1, 0);
-    if (bytes_read > 0) {
-        recv_buffer[bytes_read] = '\0';
-        printf("Received data: ");
-        for (int i = 0; i < bytes_read && i < 53; i++) {
-            putchar(recv_buffer[i]);
-        }
-        putchar('\n');
-    } else if (bytes_read < 0) {
-        perror("Receive error");
-    }
-
-    // Cleanup
-    close(client_connection);
-    close(master_socket);
-}
+#define PORT 8080  // Порт для прослушивания
+#define BUFFER_SIZE 256  // Размер буфера
 
 int main() {
-    setup_server();
-    return EXIT_SUCCESS;
+    int server_fd, client_socket;  // Дескрипторы сокетов
+    struct sockaddr_in address;  // Структура адреса
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};  // Буфер для данных
+
+    // Создание сокета
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    // Настройка адреса сервера
+    address.sin_family = AF_INET;  // IPv4
+    address.sin_addr.s_addr = INADDR_ANY;  // Все интерфейсы
+    address.sin_port = htons(PORT);  // Указанный порт
+
+    // Привязка сокета к порту
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Ошибка привязки");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Ожидание подключений
+    if (listen(server_fd, 3) < 0) {
+        perror("Ошибка ожидания");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Сервер запущен на порту %d...\n", PORT);
+
+    // Принятие подключения
+    if ((client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Ошибка принятия");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Клиент подключен!\n");
+
+    // Чтение данных от клиента
+    int valread = read(client_socket, buffer, BUFFER_SIZE);
+    printf("Получено: %.*s\n", valread, buffer);  // Вывод полученных данных
+
+    // Закрытие соединений
+    close(client_socket);
+    close(server_fd);
+    return 0;
 }
